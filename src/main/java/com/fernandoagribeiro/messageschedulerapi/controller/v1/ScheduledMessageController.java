9ab2d80f6247e6f2142601dcc816ed8f7f2706b9
Fernando Ribeiro <73204726+fernandoagribeiro@users.noreply.github.com>
2020-11-21 04:24:55 -0300

    Adding the ScheduledMessage controller
    Adding the ScheduledMessage controller.
    
    This Controller is responsible for only three tasks:
    - Save a new scheduled message
    - Query for a specific scheduled message (based on its id)
    - Delete a scheduled message (based on its id)
    
    This Controller is on its first version (v1).

---
+++ src/main/java/com/fernandoagribeiro/messageschedulerapi/controller/v1/ScheduledMessageController.java
@@ -0,0 +1,141 @@
+package com.fernandoagribeiro.messageschedulerapi.controller.v1;
+
+import com.fernandoagribeiro.messageschedulerapi.DTO.ScheduledMessageDTO;
+import com.fernandoagribeiro.messageschedulerapi.DTO.StandardResponseDTO;
+import com.fernandoagribeiro.messageschedulerapi.mapping.ScheduledMessageMapper;
+import com.fernandoagribeiro.messageschedulerapi.model.ScheduledMessage;
+import com.fernandoagribeiro.messageschedulerapi.service.MessageTypeService;
+import com.fernandoagribeiro.messageschedulerapi.service.ScheduledMessageService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/messagescheduler/v1/messages")
+@Slf4j
+public class ScheduledMessageController {
+
+    private final ScheduledMessageService scheduledMessageService;
+    private final MessageTypeService messageTypeService;
+
+    @Autowired
+    public ScheduledMessageController(ScheduledMessageService scheduledMessageService, MessageTypeService messageTypeService) {
+        this.scheduledMessageService = scheduledMessageService;
+        this.messageTypeService = messageTypeService;
+    }
+
+    private void validateMessageDTO(ScheduledMessageDTO scheduledMessageDTO) throws IllegalArgumentException {
+        if (scheduledMessageDTO.getDateToSend().isBefore(LocalDateTime.now())) {
+            throw new IllegalArgumentException("The scheduled date to send the message cannot be set in the past");
+        }
+
+        if (scheduledMessageDTO.isSent()) {
+            throw new IllegalArgumentException("It's not possible to create a message with its status being set as it" +
+                    " has already been sent");
+        }
+    }
+
+    @PostMapping("/create")
+    @ResponseBody
+    public ResponseEntity<StandardResponseDTO> create(@RequestBody @Valid ScheduledMessageDTO scheduledMessageDTO) {
+        try {
+            log.info("Ensuring that the Message Type " + scheduledMessageDTO.getMessageTypeName() + " exists on the DB");
+            var messageType = messageTypeService.findByMessageTypeName(scheduledMessageDTO.getMessageTypeName());
+
+            if (!messageType.isPresent()) {
+                var errorMessage = "The Message Type " + scheduledMessageDTO.getMessageTypeName() + " was not found";
+
+                log.error(errorMessage);
+
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new StandardResponseDTO(errorMessage, null));
+            }
+
+            log.info("Validating the Scheduled Message");
+            this.validateMessageDTO(scheduledMessageDTO);
+            log.info("The Scheduled Message is valid");
+
+            log.info("Converting the object DTO into the persistence model");
+            ScheduledMessage scheduledMessage = ScheduledMessageMapper.INSTANCE.toScheduledMessage(scheduledMessageDTO);
+            scheduledMessage.setMessageType(messageType.get());
+
+            this.scheduledMessageService.save(scheduledMessage);
+
+            var successMessage = "Scheduled Message successfully saved";
+            log.info(successMessage);
+            return ResponseEntity.ok().body(new StandardResponseDTO(successMessage, null));
+        }
+        catch (IllegalArgumentException e) {
+            log.error(e.getMessage());
+            return ResponseEntity.badRequest().body(new StandardResponseDTO(e.getMessage(),null));
+        }
+        catch (Exception e) {
+            log.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new StandardResponseDTO(e.getMessage(),
+                    null));
+        }
+    }
+
+    @GetMapping("/find/{id}")
+    @ResponseBody
+    public ResponseEntity<StandardResponseDTO> find(@PathVariable Long id) {
+        try {
+            if (id < 1)
+                throw new IllegalArgumentException("The Scheduled Message Id should be higher than zero");
+
+            var scheduledMessage = scheduledMessageService.findScheduledMessageById(id);
+
+            if (scheduledMessage.isPresent()) {
+                log.info("A Scheduled Message was found and it will now be converted to its DTO equivalent");
+                var scheduledMessageDTO = ScheduledMessageMapper.INSTANCE.toDTO(scheduledMessage.get(),
+                        scheduledMessage.get().getMessageType());
+
+                log.info("Returning the DTO object in the Response Body");
+                return ResponseEntity.ok().body(new StandardResponseDTO("Found", scheduledMessageDTO));
+            } else {
+                var errorMessage = "A scheduled message with the Id " + id + " was not found";
+                log.error(errorMessage);
+
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new StandardResponseDTO(errorMessage, null));
+            }
+        }
+        catch (IllegalArgumentException e) {
+            log.error(e.getMessage());
+            return ResponseEntity.badRequest().body(new StandardResponseDTO(e.getMessage(),null));
+        }
+        catch (Exception e) {
+            log.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new StandardResponseDTO(e.getMessage(),
+                    null));
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @ResponseBody
+    public ResponseEntity<StandardResponseDTO> delete(@PathVariable Long id) {
+        try {
+            if (id < 1)
+                throw new IllegalArgumentException("The Scheduled Message Id should be higher than zero");
+
+            log.info("Preparing to delete the Scheduled Message");
+            scheduledMessageService.deleteById(id);
+
+            var successMessage = "The Scheduled Message was successfully deleted";
+            log.info(successMessage);
+            return ResponseEntity.ok().body(new StandardResponseDTO(successMessage, null));
+        }
+        catch (IllegalArgumentException e) {
+            log.error(e.getMessage());
+            return ResponseEntity.badRequest().body(new StandardResponseDTO(e.getMessage(),null));
+        }
+        catch (Exception e) {
+            log.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new StandardResponseDTO(e.getMessage(),
+                    null));
+        }
+    }
+}
