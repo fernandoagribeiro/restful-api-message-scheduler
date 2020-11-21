d4a583d8d1d4a54f6c81e9eb4b8015543b440ae7
Fernando Ribeiro <73204726+fernandoagribeiro@users.noreply.github.com>
2020-11-21 03:55:40 -0300

    Adding the basic configuration files for a MAVEN project
    Opted to use MAVEN, and on this commit you'll find the basic files that enables this kind of project to be created.
    
    Besided Spring Boot 2.4.0, so far, I've decided to make use of the following third-party libraries:
    - Lombok 1.8.16
    - MapStruct 1.4.1.Final
    - lombok-mapstruct-binding 0.1.0. This library is necessary to make the MapStruct library and Lombok work together
    - PostgreSQL
    - Mockito 2.0.2-beta
    - log4j 1.2.17
    - Hibernate 5.3.20.Final
    
    This commit also contains the .gitignore file.

---
+++ src/main/java/com/fernandoagribeiro/messageschedulerapi/MessageSchedulerApiApplication.java
@@ -0,0 +1,13 @@
+package com.fernandoagribeiro.messageschedulerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageSchedulerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageSchedulerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fernandoagribeiro/messageschedulerapi/MessageSchedulerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandoagribeiro.messageschedulerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageSchedulerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
